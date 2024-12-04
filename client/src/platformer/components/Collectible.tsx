0461fe774b00c4eacf5e3aceef1e997da42f6d28
Cristian Brinza <cristian.brinza@isa.utm.md>
2024-12-04 16:53:59 +0200

    Add platformer game

---
+++ client/src/platformer/components/Collectible.tsx
@@ -0,0 +1,29 @@
+// src/components/Collectible.tsx
+import React from 'react';
+import styles from '../Platformer.module.css';
+
+interface CollectibleProps {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    type: 'health' | 'score';
+}
+
+const Collectible: React.FC<CollectibleProps> = ({
+                                                     x,
+                                                     y,
+                                                     width,
+                                                     height,
+                                                     type,
+                                                 }) => {
+    const color = type === 'health' ? 'red' : 'yellow';
+    return (
+        <div
+            className={styles.collectible}
+            style={{ left: x, top: y, width, height, backgroundColor: color }}
+        ></div>
+    );
+};
+
+export default Collectible;
