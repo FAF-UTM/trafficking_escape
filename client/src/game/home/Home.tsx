b8611a4022bc2bba56fec9b2e16797f161cf683d
Cristian Brinza <cristian.brinza@isa.utm.md>
2024-12-16 04:00:52 +0200

    Change start button at home route form /chat to /intro

--- client/src/game/home/Home.tsx
+++ client/src/game/home/Home.tsx
@@ -4,7 +4,7 @@ const Home: React.FC = () => {
   return (
     <div id={styles.home}>
       <div className={styles.home_button_container}>
-        <Button className={styles.home_button} variant="contained" href="/chat">
+        <Button className={styles.home_button} variant="contained" href="/intro">
           Start Game
         </Button>
         <Button
