f96d72e66bd1f5ee1cc94056ac4b266cf69b543d
CaraAlexandr <alexandru.cara@snt.md>
2025-01-18 21:45:26 +0200

    Add GET mapping to TestController, permit "/" in SecurityConfig
    - Added @GetMapping annotation to a method in TestController for handling GET requests.
    - Updated SecurityConfig to permit access to the root path ("/") along with other paths.

--- backend/src/main/java/org/example/backend/controllers/TestController.java
+++ backend/src/main/java/org/example/backend/controllers/TestController.java
@@ -2,6 +2,7 @@ package org.example.backend.controllers;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,11 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("")
 public class TestController {
-    @RequestMapping("/test")
-    public String test() {
-        return "Test successful!";
-    }
-    @RequestMapping("")
+    @GetMapping("")
     public String test2() {
         return "Backend is running!";
     }
